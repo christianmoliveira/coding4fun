0d451f240af0f341f4f8125f53f5153359780cca
christian menezes <christiannmenezes@gmail.com>
2021-01-11 16:31:46 -0300

    superzime me problem solved

--- codewars/8 kyu/supersizeMe.js
+++ codewars/8 kyu/supersizeMe.js
@@ -2,11 +2,12 @@
  * Write a function that rearranges an integer into its largest possible value.
  */
 
+//  TODO: Implementar solução
 function superSize(num){
   if (num < 10) {
     return num;
   } else {
-
+    return +num.toString().split('').sort((a, b) => b - a).join('');
   }
 }
 
@@ -14,6 +15,6 @@ const testCase1 = 69;
 const testCase2 = 513;
 const testCase3 = 2017;
 const testCase4 = 414;
-const testCase5 = 608719;
+const testCase5 = 6019;
 const testCase6 = 123456789;
 console.log(superSize(testCase1));
